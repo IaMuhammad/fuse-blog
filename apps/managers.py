d5b32fa46762f13cd4b572688f9acb29c7f6ebfe
muhammad <abduvaliyevmuhammad41@gmail.com>
2022-12-25 17:12:49 +0500

    commit all

---
+++ apps/managers.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import UserManager as BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return super().create_superuser(username, email, password, **extra_fields)
