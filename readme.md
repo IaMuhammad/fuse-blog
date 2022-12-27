87398f39fec920f9ee08919865c84fa3c6acaefd
muhammad <abduvaliyevmuhammad41@gmail.com>
2022-12-27 23:44:44 +0500

    commit all

--- readme.md
+++ readme.md
@@ -1,19 +1,31 @@
-<h1 align="center">Hi 👋, I'm Muhammad</h1>
-<h3 align="center">A passionate backend developer from Uzbekistan</h3>
+# FUSE BLOG SITE
+This is web site that posts news about IT.
 
-<p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=iamuhammad" alt="iamuhammad" /></a> </p>
+## What can FUSE BLOG
+It has many features, but here are some of the nicer ones:
+* Login and Register
+* Login & Register with your Social Account (Google)
+* CREATE, UPDATE, DELETE
+* Generates PDF version of any post that you want with qr code (it's comments included) [pdf](http://170.187.139.229:6232/pdf/abe50d4b9b5dcb583754721a786f4604)
+* Generates QR code for blog link 
+* Trending posts witch viewed the most last month
+* Responsive Pangination
+* Posts that Canceled more than 7 days ago will be deleted automatically
+* and more...
 
-- 🔭 I’m currently working on [blog](https://github.com/IaMuhammad/blog)
+## Installed apps 
+* Django
+* CKEditor
+* celery
+* Cron 
+* redis
+* PostgreSQl
+* Sentery
+* Docker
 
-- 🌱 I’m currently learning **Django Rest Framework**
 
-- 💬 You can get in touch with me by email at [abduvaliyevmuhammad41@gmail.com](mailto:abduvaliyevmuhammad41@gmail.com)
-
-<h3 align="left">Connect with me:</h3>
-<p align="left">
-<a href="https://instagram.com/mokhamed.03" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="mokhamed.03" height="30" width="40" /></a>
-<a href="https://www.leetcode.com/mamuhammad571" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/leet-code.svg" alt="mamuhammad571" height="30" width="40" /></a>
+<p align="center">
+   <img align="center" src="media/readme/img.png">
 </p>
 
-<h3 align="left">Languages and Tools:</h3>
-<p align="left"> <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> </a> <a href="https://www.djangoproject.com/" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/django.svg" alt="django" width="40" height="40"/> </a> <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/> </a> <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="40" height="40"/> </a> <a href="https://www.postgresql.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="40" height="40"/> </a> <a href="https://postman.com" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> <a href="https://redis.io" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redis/redis-original-wordmark.svg" alt="redis" width="40" height="40"/> </a> <a href="https://www.sqlite.org/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg" alt="sqlite" width="40" height="40"/> </a> </p>
+![gif]('')
--- root/settings.py
+++ root/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,7 +27,15 @@ SECRET_KEY = 'django-insecure-=9=s9i7&3d%41pp5d1&bap7*(kd4o!_0&m*arh0h*q*o$y4c5+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
 
 # Application definition
 
@@ -35,15 +46,22 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    # my apps
+    'apps.apps.AppsConfig',
 
     # installed apps
-    'django_object_actions',
     'ckeditor_uploader',
     'ckeditor',
     'fontawesomefree',
-    'fontawesome_5',
-    # my apps
-    'apps.apps.AppsConfig',
+    'django_celery_results',
+    'django_crontab',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
+    'allauth.socialaccount.providers.google',
 
 ]
 
@@ -71,12 +89,44 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'apps.context_processors.context_category',
-                'apps.context_processors.context_blog',
+                'apps.utils.context_processors.context_category',
+                'apps.utils.context_processors.context_blog',
+                'apps.utils.context_processors.context_best',
+                'apps.utils.context_processors.context_trending_posts',
+                'apps.utils.context_processors.context_site_info',
+
+                # `allauth` needs this from django
+                'django.template.context_processors.request',
             ],
         },
     },
 ]
+
+SITE_ID = 1
+
+# Provider specific settings
+SOCIALACCOUNT_PROVIDERS = {
+    'github': {
+        # For each OAuth based provider, either add a ``SocialApp``
+        # (``socialaccount`` app) containing the required client
+        # credentials, or list them here:
+        'APP': {
+            'client_id': 'ded11c3824cf1ae8d89c',
+            'secret': 'c9aa2da52fc05b26930e61ebfdb130b510e8d473',
+            'key': ''
+        }
+    },
+    'google': {
+        # For each OAuth based provider, either add a ``SocialApp``
+        # (``socialaccount`` app) containing the required client
+        # credentials, or list them here:
+        'APP': {
+            'client_id': '866914759741-qe9jemsr861go3rn57n4s8f84t3rgcpr.apps.googleusercontent.com',
+            'secret': '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',
+            'key': ''
+        }
+    }
+}
 
 WSGI_APPLICATION = 'root.wsgi.application'
 
@@ -85,15 +135,14 @@ WSGI_APPLICATION = 'root.wsgi.application'
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'blog_db',
         'USER': 'postgres',
         'PASSWORD': '1',
         'HOST': 'localhost',
         'PORT': 5432,
-
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -142,6 +191,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CKEDITOR_UPLOAD_PATH = "media/"
+
+
 
 CKEDITOR_CONFIGS = {
     'default': {
@@ -205,5 +256,48 @@ CKEDITOR_CONFIGS = {
             'dialogui',
             'elementspath'
         ]),
+
     }
 }
+
+# CELERY SETTINGS
+
+CELERY_TIMEZONE = 'Asia/Tashkent'
+CELERY_ACCEPT_CONTEN = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+
+CELERY_RESULT_BACKEND = 'django-db'
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'djangoemail.fuse@gmail.com'
+EMAIL_HOST_PASSWORD = 'deujmvbkymobzcmk'
+
+DEFAULT_FROM_EMAIL = 'Celery <djangoemail.fuse@gmail.com>'
+
+CRONJOBS = [
+    ('0 0 * * *', 'apps.utils.cron.delete_blog')
+]
+
+sentry_sdk.init(
+    dsn="https://ce866baa02a44cd7adc288212bc5c7ae@o4504382011015168.ingest.sentry.io/4504382037622784",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
